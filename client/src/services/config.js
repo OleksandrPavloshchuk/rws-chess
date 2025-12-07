6c7d90f51e2c816128aa5c5e4d94c5821b2ecd1f
Oleksandr Pavloshchuk <oleksandr.pavloshchuk@openiam.com>
2025-12-07 19:49:17 +0200

    small changes

--- client/src/services/config.js
+++ client/src/services/config.js
@@ -1,7 +1,7 @@
 export default class Configuration {
   constructor() {
     //this.webSocketUrl = "ws://10.91.161.109:3016/ws";
-    this.webSocketUrl = "ws://localhost:3006/ws";
+    this.webSocketUrl = "ws://localhost:3016/ws";
     //this.webSocketUrl = "ws://192.168.1.9:3016/ws";
   }
 }
